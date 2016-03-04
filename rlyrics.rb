d56109ab6c96c2f6cbcc9350305d70b3ef033d0c
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2016-03-05 01:11:54 +0200

    workaround to save lyrics fetched from query search

--- rlyrics.rb
+++ rlyrics.rb
@@ -46,12 +46,31 @@ if Check.get_type(query) == 1
 		}	
 	end	
 else
-	song = get_lyrics(query)
-	p song
-	song.lyrics.each{|line| 
-		if !line.match("Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that.")
-		print line.gsub("NEWLINE","\n")
-		end}
+        #Requires internet connection, how dumb! 
+        song = get_lyrics(query)
+        lyrics = Lyrics.search(song,"lyrics.json")	
+        if lyrics.size == 0
+		song.lyrics.each{|line| 
+			if !line.match("Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that.")
+			print line.gsub("NEWLINE","\n")
+			end}
+		p "Do you want to save the lyrics?[y,n]"
+		ans = STDIN.gets
+		if ans.chomp == "y"
+			Lyrics.save(song,"lyrics.json")
+		end
+	else
+		p song
+		lyrics.each {|line|
+		       #print line.gsub("NEWLINE","\n")	
+			if line == "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that."
+				#Do Nothing
+			else
+					print line.gsub("NEWLINE","\n")
+		
+			end
+		}	
+	end	
 
 end
 
