13a7e432a7cad282d9875a255890e15be558f869
Ahmed Khaled <ahmed.khaled1512@yahoo.com>
2016-03-05 12:10:14 +0200

    ask_to_save() and print_lyrics()

--- rlyrics.rb
+++ rlyrics.rb
@@ -2,6 +2,11 @@ load 'check.rb'
 load 'fetch.rb'
 load 'id3.rb'
 load 'lyrics.rb'
+load 'song.rb'
+
+file = 1
+search = 0
+line = "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that."
 
 def get_lyrics(query,my_song=nil)
 
@@ -13,9 +18,33 @@ def get_lyrics(query,my_song=nil)
 	return song
 end
 
+def ask_to_save(song)
+		p "Do you want to save the lyrics?[y,n]"
+		ans = STDIN.gets
+		if ans.chomp == "y"
+			Lyrics.save(song,"lyrics.json")
+		end
+end
 
-file = 1
-search = 0
+def print_lyrics(lyrics_arr)
+    if lyrics_arr.class == Song
+        new_song.lyrics.each{|line| 
+			if !line.match(line)
+			print line.gsub("NEWLINE","\n")
+			end}
+    else
+        lyrics.each {|line|
+		       #print line.gsub("NEWLINE","\n")	
+			if line == line
+				#Do Nothing
+			else
+					print line.gsub("NEWLINE","\n")
+		
+			end
+    end
+end
+
+
 query = ARGV.join("+")
 
 if Check.get_type(query) == 1
@@ -24,51 +53,22 @@ if Check.get_type(query) == 1
 	if lyrics.size == 0
 		search_text = "#{song.artist}+#{song.title}"
 		new_song = get_lyrics(search_text,song)
-		new_song.lyrics.each{|line| 
-			if !line.match("Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that.")
-			print line.gsub("NEWLINE","\n")
-			end}
-		p "Do you want to save the lyrics?[y,n]"
-		ans = STDIN.gets
-		if ans.chomp == "y"
-			Lyrics.save(new_song,"lyrics.json")
-		end
+		print_lyrics(new_song)
+                ask_to_save(new_song)
 	else
 		p song
-		lyrics.each {|line|
-		       #print line.gsub("NEWLINE","\n")	
-			if line == "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that."
-				#Do Nothing
-			else
-					print line.gsub("NEWLINE","\n")
-		
-			end
-		}	
+		print_lyrics(lyrics)	
 	end	
 else
         #Requires internet connection, how dumb! 
         song = get_lyrics(query)
         lyrics = Lyrics.search(song,"lyrics.json")	
         if lyrics.size == 0
-		song.lyrics.each{|line| 
-			if !line.match("Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that.")
-			print line.gsub("NEWLINE","\n")
-			end}
-		p "Do you want to save the lyrics?[y,n]"
-		ans = STDIN.gets
-		if ans.chomp == "y"
-			Lyrics.save(song,"lyrics.json")
-		end
+		print_lyrics(song)
+		ask_to_save(song)
 	else
 		p song
-		lyrics.each {|line|
-		       #print line.gsub("NEWLINE","\n")	
-			if line == "Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that."
-				#Do Nothing
-			else
-					print line.gsub("NEWLINE","\n")
-		
-			end
+		print_lyrics(lyrics)
 		}	
 	end	
 
